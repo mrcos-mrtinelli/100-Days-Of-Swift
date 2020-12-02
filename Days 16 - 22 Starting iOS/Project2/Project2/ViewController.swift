3d2f9795c11b0eca3eb17cbca2e8ab236b519a0c
mrkos-mrtinelli <m2e5090@gmail.com>
2020-12-01 16:06:11 -0800

    project 2, part two

--- Days 16 - 22 Starting iOS/Project2/Project2/ViewController.swift
+++ Days 16 - 22 Starting iOS/Project2/Project2/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,12 +32,32 @@ class ViewController: UIViewController {
         askQuestion()
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
+        title = countries[correctAnswer].uppercased()
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
     }
-
-
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var alertTitle: String
+        
+        if sender.tag == correctAnswer {
+            alertTitle = "Correct!"
+            score += 1
+        } else {
+            alertTitle = "Incorrect."
+            score -= 1
+        }
+        let ac = UIAlertController(title: alertTitle, message: "Your score is \(score)", preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: alertTitle, style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
+    }
+    
 }
 
