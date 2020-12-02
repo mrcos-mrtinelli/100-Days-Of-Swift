6bc5e3fe56c327cdb97286808848a8942ed21fad
mrkos-mrtinelli <m2e5090@gmail.com>
2020-12-02 11:30:50 -0800

    day 21, completed challenge

--- Days 16 - 22 Starting iOS/Project2/Project2/ViewController.swift
+++ Days 16 - 22 Starting iOS/Project2/Project2/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     var countries = [String]()
     var score = 0
     var correctAnswer = 0
+    var totalQuestionsAsked = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,8 +36,9 @@ class ViewController: UIViewController {
     func askQuestion(action: UIAlertAction! = nil) {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+        totalQuestionsAsked += 1
         
-        title = countries[correctAnswer].uppercased()
+        title = "\(countries[correctAnswer].uppercased()) (Score: \(score))"
         
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
@@ -44,17 +46,32 @@ class ViewController: UIViewController {
     }
     @IBAction func buttonTapped(_ sender: UIButton) {
         var alertTitle: String
+        var message: String
+        var alertAction = "Continue"
         
         if sender.tag == correctAnswer {
             alertTitle = "Correct!"
             score += 1
+            message = "Your score is \(score)"
         } else {
             alertTitle = "Incorrect."
             score -= 1
+            message = "That is the flag for \(countries[sender.tag].uppercased()) "
         }
-        let ac = UIAlertController(title: alertTitle, message: "Your score is \(score)", preferredStyle: .alert)
         
-        ac.addAction(UIAlertAction(title: alertTitle, style: .default, handler: askQuestion))
+        if totalQuestionsAsked == 3 {
+            alertTitle = "Game Over"
+            message = "Your final score is \(score)/\(totalQuestionsAsked)"
+            alertAction = "Play again!"
+            score = 0
+            totalQuestionsAsked = 0
+        }
+        showStats(title: alertTitle, message: message, alertAction: alertAction)
+        
+    }
+    func showStats(title: String, message: String, alertAction: String) {
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: alertAction, style: .default, handler: askQuestion))
         
         present(ac, animated: true)
     }
