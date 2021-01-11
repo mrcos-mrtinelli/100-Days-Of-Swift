c2488920fa2c9c0fb1f5c279edfb531eb06db444
mrkos-mrtinelli <m2e5090@gmail.com>
2021-01-10 17:43:32 -0800

    fetch api and present it in tableview

---
+++ Days 52-59 Images and animation/Day 59 Consolidation Vi/Projects13-15/Projects13-15/Country.swift
@@ -0,0 +1,15 @@
+//
+//  Countries.swift
+//  Projects13-15
+//
+//  Created by Marcos Martinelli on 1/10/21.
+//
+
+import Foundation
+
+struct Country: Codable {
+    var flag: String
+    var region: String
+    var name: String
+    var capital: String
+}
--- Days 52-59 Images and animation/Day 59 Consolidation Vi/Projects13-15/Projects13-15/ViewController.swift
+++ Days 52-59 Images and animation/Day 59 Consolidation Vi/Projects13-15/Projects13-15/ViewController.swift
@@ -8,27 +8,72 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    var countries = [Country]()
+    var regions = [String]()
+    var countriesByRegion = [String: [Country]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Coutry Facts"
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // make this run in the background
+        let urlString = "https://restcountries.eu/rest/v2/all"
+        
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                }
+                if let unwrappedData = data {
+                    self.countries = self.parseCountries(unwrappedData)
+                    
+                    DispatchQueue.main.async {
+                        for country in self.countries {
+                            if !self.regions.contains(country.region) {
+                                self.regions.append(country.region)
+                            }
+                        }
+                        
+                        self.tableView.reloadData()
+                    }
+                    
+                }
+            }
+            task.resume()
+        }
+        // end background
     }
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
-    }
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        return regions.count
+//    }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return countries.count
     }
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        let title = "Section"
-        return title
-    }
+//    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        let title = regions[section]
+//        return title
+//    }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CountryCell", for: indexPath)
-        cell.textLabel?.text = "Country"
+        
+        
+        cell.textLabel?.text = countries[indexPath.row].name
         return cell
+    }
+    
+    func parseCountries(_ jsonData: Data) -> [Country] {
+        let decoder = JSONDecoder()
+        
+        do {
+            let decodedData = try decoder.decode([Country].self, from: jsonData)
+            return decodedData
+        } catch {
+            print(error)
+        }
+        return [Country]()
     }
 }
 
