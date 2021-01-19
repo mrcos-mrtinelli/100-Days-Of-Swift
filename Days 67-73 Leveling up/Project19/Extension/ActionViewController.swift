1b9ed43b4478f6874736c31b67d44f2527ca148c
mrkos-mrtinelli <m2e5090@gmail.com>
2021-01-18 16:32:32 -0800

    day 68, finish extension

--- Days 67-73 Leveling up/Project19/Extension/ActionViewController.swift
+++ Days 67-73 Leveling up/Project19/Extension/ActionViewController.swift
@@ -10,10 +10,20 @@ import MobileCoreServices
 
 class ActionViewController: UIViewController {
 
-    @IBOutlet weak var imageView: UIImageView!
-
+    @IBOutlet var script: UITextView!
+    
+    var pageTitle = ""
+    var pageURL = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+        
+        // add KVO to detect iphone keyboard changes
+        let notificationCenter = NotificationCenter.default
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+        notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
     
         // extensionContext is an intermediary between Safari and our extension
         // get first item for inputItem from inputItems array, cast it as NDExtensionItem
@@ -22,16 +32,46 @@ class ActionViewController: UIViewController {
             // inputItem is an NSExtension, get first item in .attachments array
             if let itemProvider = inputItem.attachments?.first {
                 itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [weak self] (dict, error) in
-                    // data has been loaded, code below manipulates the data
+                    guard let itemDict = dict as? NSDictionary else { return }
+                    guard let jsValues = itemDict[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary else { return }
+                    
+                    self?.pageTitle = jsValues["title"] as? String ?? ""
+                    self?.pageURL = jsValues["url"] as? String ?? ""
+                    
+                    DispatchQueue.main.async {
+                        self?.title = self?.pageTitle
+                    }
                 }
             }
         }
     }
+    
+    @objc func adjustForKeyboard(notification: Notification) {
+        guard let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+        
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            script.contentInset = .zero
+        } else {
+            script.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: keyboardViewEndFrame.height - view.safeAreaInsets.bottom, right: 0)
+        }
+        
+        script.scrollIndicatorInsets = script.contentInset
+        
+        let selectedRange = script.selectedRange
+        script.scrollRangeToVisible(selectedRange)
+    }
 
     @IBAction func done() {
-        // Return any edited content to the host app.
-        // This template doesn't do anything, so we just echo the passed in items.
-        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+        let item = NSExtensionItem()
+        let argument: NSDictionary = ["customJavaScript": script.text]
+        let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
+        let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
+        item.attachments = [customJavaScript]
+        
+        extensionContext?.completeRequest(returningItems: [item])
     }
 
 }
