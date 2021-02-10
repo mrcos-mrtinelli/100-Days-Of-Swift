b9f59a1f3412ab0871de3023916d6a14fa169e69
mrkos-mrtinelli <m2e5090@gmail.com>
2021-02-09 18:51:02 -0800

    added UIImagePickerController and prelim filter

--- Days 90-91 Consolidation X/Milestone Projects 25-27/Milestone Projects 25-27/ViewController.swift
+++ Days 90-91 Consolidation X/Milestone Projects 25-27/Milestone Projects 25-27/ViewController.swift
@@ -8,6 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    var newImage: UIImage!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +28,71 @@ class ViewController: UIViewController {
         navigationController?.isToolbarHidden = false
     }
     @objc func importPictureTapped() {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let picker = UIImagePickerController()
+            
+            picker.delegate = self
+            picker.allowsEditing = true
+            
+            popoverPresentationController?.barButtonItem = toolbarItems![0]
+            present(picker, animated: true)
+            
+        } else {
+            print("No access to photoLibrary")
+            
+        }
+    }
+    func textRenderer() {
+        let imgWidth: CGFloat = newImage.size.width
+        let imgHeight: CGFloat = newImage.size.height
+        let renderer = UIGraphicsImageRenderer(
+            size: CGSize(
+                width: imgWidth,
+                height: imgHeight
+            )
+        )
+        let overlay = renderer.image { (context) in
+            let overlayArea = CGRect(x: 0, y: 0, width: imgWidth, height: imgHeight)
+            
+            let paragraphStyle = NSMutableParagraphStyle()
+            
+            paragraphStyle.alignment = .center
+            
+            let attributes: [NSAttributedString.Key: Any] = [
+                .font: UIFont.systemFont(ofSize: 72, weight: .bold),
+                .paragraphStyle: paragraphStyle
+            ]
+            let string = "This is a test."
+            let attributedString = NSAttributedString(string: string, attributes: attributes)
+            
+            attributedString.draw(
+                with: CGRect(x: 0, y: 0, width: imgWidth, height: 100),
+                options: .usesLineFragmentOrigin,
+                context: nil
+            )
+            
+            newImage.draw(in: overlayArea, blendMode: .overlay, alpha: 1)
+        }
         
+        imageView.image = overlay
+    }
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let selectedImage = info[.editedImage] as? UIImage else {
+            
+            print("no image found")
+            
+            return
+        }
+        
+        newImage = selectedImage
+//        imageView.image = selectedImage
+        textRenderer()
+        
+        dismiss(animated: true)
     }
 }
 
