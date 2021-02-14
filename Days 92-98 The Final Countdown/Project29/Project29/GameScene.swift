4f58c2764e3eb687338b48924d7a37e353c38728
mrkos-mrtinelli <m2e5090@gmail.com>
2021-02-13 19:29:01 -0800

    day 94, added project skeleton

---
+++ Days 92-98 The Final Countdown/Project29/Project29/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  Project29
+//
+//  Created by Marcos Martinelli on 2/13/21.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+    }
+    
+}
