c99f5f413f6707c157e965794413d27f60feac77
mrkos-mrtinelli <m2e5090@gmail.com>
2021-02-14 12:54:40 -0800

    catch up on yesterday's work

--- Days 92-98 The Final Countdown/Project29/Project29/GameViewController.swift
+++ Days 92-98 The Final Countdown/Project29/Project29/GameViewController.swift
@@ -10,7 +10,15 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
+    weak var currentGame: GameScene?
+    
+    @IBOutlet var angleSlider: UISlider!
+    @IBOutlet var angleLabel: UILabel!
+    @IBOutlet var velocitySlider: UISlider!
+    @IBOutlet var velocityLabel: UILabel!
+    @IBOutlet var launchButton: UIButton!
+    @IBOutlet var playerNumber: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,12 +30,18 @@ class GameViewController: UIViewController {
                 
                 // Present the scene
                 view.presentScene(scene)
+                
+                currentGame = scene as? GameScene
+                currentGame?.viewController = self
             }
             
             view.ignoresSiblingOrder = true
             
             view.showsFPS = true
             view.showsNodeCount = true
+            
+            angleChanged(self)
+            velocityChanged(self)
         }
     }
 
@@ -45,5 +59,38 @@ class GameViewController: UIViewController {
 
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+    
+    @IBAction func angleChanged(_ sender: Any) {
+        angleLabel.text = "Angle: \(Int(angleSlider.value))°"
+    }
+    @IBAction func velocityChanged(_ sender: Any) {
+        velocityLabel.text = "Velocity: \(Int(velocitySlider.value))"
+    }
+    @IBAction func launch(_ sender: Any) {
+        angleSlider.isHidden = true
+        angleLabel.isHidden = true
+        
+        velocityLabel.isHidden = true
+        velocitySlider.isHidden = true
+        
+        launchButton.isHidden = true
+        
+        currentGame?.launch(angle: Int(angleSlider.value), velocity: Int(velocitySlider.value))
+    }
+    func activatePlayer(number: Int) {
+        if number == 1 {
+            playerNumber.text = "<<< PLAYER ONE"
+        } else {
+            playerNumber.text = "PLAYER TWO >>>"
+        }
+        angleSlider.isHidden = false
+        angleLabel.isHidden = false
+        
+        velocityLabel.isHidden = false
+        velocitySlider.isHidden = false
+        
+        launchButton.isHidden = false
+        
     }
 }
